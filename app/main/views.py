ac35be43c3134eb4c97cb73fcf11273eb49933d6
Tian Xie <xietian@outlook.com>
2016-01-08 22:35:46 +0800

    Add views

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
-
 from . import main
 from .forms import NameForm
 from .. import db
@@ -11,8 +10,15 @@ from ..models import User
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        # ...
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
         return redirect(url_for('.index'))
     return render_template('index.html',
-                           form=form, name=session.get('known', False),
-                           current_time=datetime.utcnow())
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
