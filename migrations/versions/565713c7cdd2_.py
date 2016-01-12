6f77464ce064732ac87329dc851077dd1c567a7c
Tian Xie <xietian@outlook.com>
2016-01-12 22:19:14 +0800

    Add user authentication

---
+++ migrations/versions/565713c7cdd2_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 565713c7cdd2
+Revises: 3bca8597d7cc
+Create Date: 2016-01-12 21:39:09.204577
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '565713c7cdd2'
+down_revision = '3bca8597d7cc'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
